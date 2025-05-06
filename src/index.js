62b0e1baa2b2a1db9aa612f18f31cfca01f489cc
Grant Watson <grant@gwsapp.net>
2025-05-05 22:35:06 -0400

    feat: Update app manifest and service worker registration
    - Changed app name and short name in manifest.json to "Recipe Manager" and "Recipes" respectively.
    - Updated theme color in manifest.json to white.
    - Added meta tags for Apple mobile web app capabilities in index.html.
    - Registered service worker in index.js for improved offline support.
    - Created serviceWorkerRegistration.js to handle service worker registration logic.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,3 +14,4 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+serviceWorkerRegistration.register();
